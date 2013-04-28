d0b42b5adf6eee146311d7a2917618162d9f7cfe
eric <h.minghe@gmail.com>
2013-04-28 20:09:28 +0800

    found out umts interface only for sending, can not be used for recieving

--- examples/new.tcl
+++ examples/new.tcl
@@ -8,6 +8,7 @@ if {$argc != 0} {
 global ns
 
 source ./function.tcl
+source ./topo.tcl
 
 # set output directory
 set output_dir .
@@ -16,14 +17,12 @@ set output_dir .
 set ns [new Simulator]
 #$ns use-newtrace
 
-$ns color 1 Blue
-$ns color 2 Red
-
 #set trace output file and directory
 set f [open out.tr w]
 $ns trace-all $f
 set namfile [open out.nam w]
-$ns namtrace-all-wireless $namfile 1000 1000
+#$ns namtrace-all-wireless $namfile 1000 1000
+$ns namtrace-all $namfile
 
 # configure UMTS 
 $ns set hsdschEnabled_ 1addr
@@ -32,18 +31,20 @@ $ns set hsdsch_rlc_nif_ 0
 
 # configure address model (domain:cluster:ip number) (needed for routing over a basestation)
 $ns node-config -addressType hierarchical
-AddrParams set domain_num_  25           	           												;# domain number
-AddrParams set cluster_num_ {1  1 1 1  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1}            		;# cluster number
-AddrParams set nodes_num_   {22 1 1 21 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1}	      			;# nodes number             
+AddrParams set domain_num_  31           	           												;# domain number
+AddrParams set cluster_num_ {1  1 1 1 1 1 1 1 1 1 1  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1}      	;# cluster number
+AddrParams set nodes_num_   {22 1 1 1 1 1 1 1 1 1 21 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1}	  	;# nodes number             
 
 #configure RNC 
 puts ""
 puts "Creating RNC"
 $ns node-config -UmtsNodeType rnc 
 set RNC [$ns create-Umtsnode 0.0.0] ;# node id is 0.
-setNodePosition $RNC 550 500 0 
+$ns at 0.005 "$RNC label \"RNC 0.0.0\""
+
+setNodePosition $RNC $opt(RNC_X) $opt(RNC_Y) 0 
 $RNC color "yellow"
-$ns initial_node_pos $RNC 20
+$ns initial_node_pos $RNC 5
 printNodeID $RNC "RNC"
 puts "Done RNC configuration"
 
@@ -59,14 +60,16 @@ $ns node-config -UmtsNodeType bs \
       	-hs_downlinkBW 384kbs 
 
 set BS [$ns create-Umtsnode 0.0.1] ;# node id is 1, BS and RNC is in the same domain
-setNodePosition $BS 580 500 0
-$BS color "blue"
-$ns initial_node_pos $BS 10
+$ns at 0.005 "$BS label \"BS 0.0.1\""
+
+setNodePosition $BS $opt(BS_X) $opt(BS_Y) 0
+$BS color yellow
+$ns initial_node_pos $BS 400
 printNodeID $BS "BS"
 puts "Done BS configuration"
 
 # Link BS and RNC 
-$ns setup-Iub $BS $RNC 622Mbit 622Mbit 15ms 15ms DummyDropTail 2000
+$ns setup-Iub $BS $RNC 622Mbit 622Mbit 15ms 15ms DummyDropTail 2000 color red
 
 #create UMTS nodes
 puts ""
@@ -76,23 +79,16 @@ $ns node-config -UmtsNodeType ue \
 		-radioNetworkController $RNC
 
 for {set i 0} {$i < 20} {incr i 1} {
-	set umts_ue($i) [$ns create-Umtsnode 0.0.[expr $i+2]]
-	setRandomPositionForNode $umts_ue($i) 500 600
-#	printNodeID $umts_ue($i) "umts_ue($i)"
-#	printNodePosition $umts_ue($i)
-	$umts_ue($i) color "red"
-	$ns initial_node_pos $umts_ue(0) 0.5
-	#umtsNodeMove $umts_ue($i) 1.0 0.2 5.0
+	set td_scdma_interface($i) [$ns create-Umtsnode 0.0.[expr $i+2]]
+	
+#	$ns at 0.005 "$td_scdma_interface($i) label \"td_scdma_interface($i) 0.0.[expr $i+2]\""
+	setRandomPositionForNode $td_scdma_interface($i) 400 600
+	$ns initial_node_pos $td_scdma_interface($i) $opt(node_size)
+	$td_scdma_interface($i) color "red"
 }
 
 # create a dummy node for simulation run normally, but
 # it seems no need anymore
-
-#set dummy_node [$ns create-Umtsnode 0.0.3] ;# node id is 3
-#$dummy_node set Y_ 150
-#$dummy_node set X_ 100 
-#$dummy_node set Z_ 0
-#$dummy_node color "brown"
 
 # Create SGSN and GGSN, they are in different domain and id for SGSN0 and GGSN0 are 4 and 5, respectively.
 
@@ -100,16 +96,18 @@ puts ""
 puts "Creating SGSN and GGSN"
 
 set SGSN [$ns node 1.0.0]
-setNodePosition $SGSN 590 500 0 
+$ns at 0.005 "$SGSN label \"SGSN 1.0.0\""
+setNodePosition $SGSN $opt(SGSN_X) $opt(SGSN_Y) 0
 printNodeID $SGSN "SGSN"
 $SGSN color "blue"
-$ns initial_node_pos $SGSN 10
+$ns initial_node_pos $SGSN $opt(node_size)
 
 set GGSN [$ns node 2.0.0]
-setNodePosition $GGSN 600 600 0
+$ns at 0.005 "$GGSN label \"GGSN 2.0.0\""
+setNodePosition $GGSN $opt(GGSN_X) $opt(GGSN_Y) 0
 printNodeID $GGSN "GGSN"
 $GGSN color "blue"
-$ns initial_node_pos $GGSN 10
+$ns initial_node_pos $GGSN $opt(node_size)
 
 puts "Done SGSN and GGSN creation"
 
@@ -119,11 +117,40 @@ puts "Done SGSN and GGSN creation"
 puts ""
 puts "Creating core-network node"
 
-set CN_host0 [$ns node 4.0.0]
-setNodePosition $CN_host0 600 500 0 
-printNodeID $CN_host0 "CN_host0"
-$CN_host0 color "black"
-$ns initial_node_pos $CN_host0 5
+set Yahoo [$ns node 3.0.0]
+$ns at 0.005 "$Yahoo label \"Yahoo\""
+setNodePosition $Yahoo $opt(Yahoo_X) $opt(Yahoo_Y) 0
+$ns initial_node_pos $Yahoo $opt(node_size)
+
+set Youtube [$ns node 4.0.0]
+$ns at 0.005 "$Youtube label \"Youtube\""
+setNodePosition $Youtube $opt(Youtube_X) $opt(Youtube_Y) 0
+$ns initial_node_pos $Youtube $opt(node_size)
+
+set Facebook [$ns node 5.0.0]
+$ns at 0.005 "$Facebook label \"Facebook\""
+setNodePosition $Facebook $opt(Facebook_X) $opt(Facebook_Y) 0
+$ns initial_node_pos $Facebook $opt(node_size)
+
+set QQ [$ns node 6.0.0]
+$ns at 0.005 "$QQ label \"QQ\""
+setNodePosition $QQ $opt(QQ_X) $opt(QQ_Y) 0
+$ns initial_node_pos $QQ $opt(node_size)
+
+set JingFm [$ns node 7.0.0]
+$ns at 0.005 "$JingFm label \"JingFm\""
+setNodePosition $JingFm $opt(JingFm_X) $opt(JingFm_Y) 0
+$ns initial_node_pos $JingFm $opt(node_size)
+
+set Steam [$ns node 8.0.0]
+$ns at 0.005 "$Steam label \"Steam\""
+setNodePosition $Steam $opt(Steam_X) $opt(Steam_Y) 0
+$ns initial_node_pos $Steam $opt(node_size)
+
+set Google [$ns node 9.0.0]
+$ns at 0.005 "$Google label \"Google\""
+setNodePosition $Google $opt(Google_X) $opt(Google_Y) 0
+$ns initial_node_pos $Google $opt(node_size)
 
 puts "Done core-network node creation"
 
@@ -131,9 +158,16 @@ puts "Done core-network node creation"
 puts ""
 puts "Connecting RNC SGSN GGSN CN_host0"
 
-$ns duplex-link $RNC $SGSN 622Mbit 0.4ms DropTail 1000
-$ns duplex-link $SGSN $GGSN 622MBit 10ms DropTail 1000
-$ns duplex-link $GGSN $CN_host0 10MBit 15ms DropTail 1000
+$ns duplex-link $RNC $SGSN 622Mbit 0.4ms DropTail 1000 color red
+$ns duplex-link $SGSN $GGSN 622MBit 10ms DropTail 1000 color red
+$ns duplex-link $GGSN $Yahoo 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $Youtube 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $Facebook 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $QQ 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $JingFm 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $Steam 10MBit 15ms DropTail 1000 color red
+$ns duplex-link $GGSN $Google 10MBit 15ms DropTail 1000 color red
+$RNC add-gateway $SGSN
 
 puts "Done connection of RNC, SGSN GGSN CN_host0"
 
@@ -157,8 +191,8 @@ set opt(ant)            Antenna/OmniAntenna        			;# antenna model 802.11
 set opt(ifqlen)         50              	   			    ;# max packet in ifq 802.11
 set opt(adhocRouting)   DSDV                       			;# routing protocol 802.11
 set opt(umtsRouting)    ""                         			;# routing for UMTS (to reset node config)
-set opt(x) 				1000   			                    ;# X dimension of the topography
-set opt(y) 				1000		                        ;# Y dimension of the topography
+set opt(x) 				2500   			                    ;# X dimension of the topography
+set opt(y) 				2500		                        ;# Y dimension of the topography
 
 Mac/802_11 set basicRate_ 11Mb
 Mac/802_11 set dataRate_ 11Mb
@@ -171,15 +205,19 @@ $topo load_flatgrid $opt(x) $opt(y)
 puts "Topology created"
 
 # create God
-set god [create-god 66]				                		     ;# give the number of nodes 
+#set god [create-god 92]				                		     ;# give the number of nodes 
+create-god 72
 
 # Create multiple interfaces nodes
 puts ""
 puts "Creating multiple interfaces ue"
 $ns node-config  -multiIf ON                            		;#to create MultiFaceNode
 for {set i 0} {$i < 20} {incr i 1} {
-	set ue($i) [$ns node [expr 5+$i].0.0]						;# multiIf has not X_ and Y_
+	set ue($i) [$ns node [expr 11+$i].0.0]						;# multiIf has not X_ and Y_
+	$ns at 0.005 "$ue($i) label \"ue($i) [expr 10+$i].0.0]\""	
 	set in_service_network($i) $NO_SERVICE
+
+	#$ns initial_node_pos $ue($i) 1
 }
 $ns node-config  -multiIf OFF                           		;#reset attribute
 puts "Done multiIf node creation"
@@ -188,9 +226,8 @@ puts "Done multiIf node creation"
 #$god set-dist 1 2 1 
 #$god set-dist 0 2 2 
 #$god set-dist 0 1 1 
-set god [God instance]
+#set god [God instance]
 
-#配置接入点AP 
 puts "Creating AP"
 puts ""
 puts "coverge:70m"
@@ -210,17 +247,17 @@ $ns node-config  -adhocRouting $opt(adhocRouting) \
                  -phyType $opt(netif) \
                  -topoInstance $topo \
                  -wiredRouting ON \
-                 -agentTrace OFF\
-                 -routerTrace OFF \
                  -macTrace ON  \
-                 -movementTrace OFF	
+                 -movementTrace ON	
 
 # configure Base station 802.11
-set AP0 [$ns node 3.0.0]
-setNodePosition $AP0 500 500 0 
+set AP0 [$ns node 10.0.0]
+$ns at 0.005 "$AP0 label \"AP0 3.0.0\""
+
+setNodePosition $AP0 $opt(AP_X) $opt(AP_Y) 0 
 printNodeID $AP0 "AP0"
-$AP0 color "red"
-$ns initial_node_pos $AP0 10
+$AP0 color red
+$ns initial_node_pos $AP0 50
 
 [$AP0 set mac_(0)] bss_id [[$AP0 set mac_(0)] id]
 [$AP0 set mac_(0)] enable-beacon
@@ -234,14 +271,19 @@ puts "Creating WLAN UEs"
 $ns node-config -wiredRouting OFF \
                 -macTrace ON 				
 for {set i 0} {$i < 20} {incr i 1} {
-	set wlan_ue($i) [$ns node 3.0.[expr $i+1]]
-	$wlan_ue($i) random-motion 1
-	$wlan_ue($i) base-station [AddrParams addr2id [$AP0 node-addr]]
-	setRandomPositionForNode $wlan_ue($i) [expr 500-$i*20] [expr 500+$i*20]
-	syncTwoInterfaces $wlan_ue($i) $umts_ue($i)
-	printNodeID $wlan_ue($i) "wlan_ue($i)"
-#	printNodePosition $wlan_ue($i)
-	$ns initial_node_pos $wlan_ue($i) 0.5
+	set wifi_interface($i) [$ns node 10.0.[expr $i+1]]
+	$wifi_interface($i) random-motion 1
+#	$ns at 0.005 "$wifi_interface($i) label \"wifi_interface($i) 3.0.[expr $i+1]\""
+
+	$wifi_interface($i) base-station [AddrParams addr2id [$AP0 node-addr]]
+	setRandomPositionForNode $wifi_interface($i) [expr 460-$i*20] [expr 500+$i*20]
+	syncTwoInterfaces $wifi_interface($i) $td_scdma_interface($i)
+
+	#[$wifi_interface($i) set mac_(0)] set-channel 1
+	printNodeID $wifi_interface($i) "wifi_interface($i)"
+#	printNodePosition $wifi_interface($i)
+	$ns initial_node_pos $wifi_interface($i) $opt(node_size)
+	$td_scdma_interface($i) color blue
 }
 puts "Done creation of WLAN UEs"
 # add link to backbone
@@ -254,8 +296,8 @@ puts "Done connection of AP0 and GGSN"
 puts ""
 puts "adding interfaces to multiface ue"
 for {set i 0} {$i < 20} {incr i 1} {
-	$ue($i) add-interface-node $wlan_ue($i)
-	$ue($i) add-interface-node $umts_ue($i)
+	$ue($i) add-interface-node $wifi_interface($i)
+	$ue($i) add-interface-node $td_scdma_interface($i)
 }
 puts "Done adding interfaces to multiface ue"
 
@@ -265,18 +307,17 @@ set realtime_monitor [new Agent/RealtimeTrace]
 
 for {set i 0} {$i < 20} {incr i 1} {
 	set src_umts($i)  [new Agent/UDP]
-	$ue($i) attach-agent $src_umts($i) $umts_ue($i)
+	$ue($i) attach-agent $src_umts($i) $td_scdma_interface($i)
 	set sink_umts($i) [new Agent/Null]
-	#$ue(0) attach-agent $sink(0) $umts_ue(0) 
-    $ns attach-agent $umts_ue($i) $sink_umts($i)
+	#$ue(0) attach-agent $sink(0) $td_scdma_interface(0) 
+    $ns attach-agent $td_scdma_interface($i) $sink_umts($i)
     $src_umts($i) set fid_ $i
     $sink_umts($i) set fid_ $i
 
     set src_wlan($i)  [new Agent/UDP]
-    $ue($i) attach-agent $src_wlan($i) $wlan_ue($i)
+    $ue($i) attach-agent $src_wlan($i) $wifi_interface($i)
     set sink_wlan($i) [new Agent/Null]
-    #$ue(0) attach-agent $sink(0) $umts_ue(0) 
-    $ns attach-agent $wlan_ue($i) $sink_wlan($i)
+    $ns attach-agent $wifi_interface($i) $sink_wlan($i)
     $src_wlan($i) set fid_ [expr $i+20]
     $sink_wlan($i) set fid_ [expr $i+20]
 }
@@ -285,75 +326,41 @@ for {set i 0} {$i < 20} {incr i 1} {
 	set app($i) [new Application/Traffic/CBR]
 }
 
-#$ns connect $src_umts(0) $sink_wlan(1)
-#puts [getNodeIpAddress $umts_ue(0)]
-#puts [getNodeIpAddress $wlan_ue(1)]
-#puts "----"
-#set e_app(0) [new Application/Traffic/CBR]
-#$e_app(0) attach-agent $src_umts(0)
-#$e_app(0) set packet_size_ 666
-#$e_app(0) set type_ CBR
-
-#$ns connect $src_wlan(1) $sink_umts(2)
-#puts [getNodeIpAddress $wlan_ue(1)]
-#puts [getNodeIpAddress $umts_ue(2)]
-#puts "----"
-
-#set e_app(1) [new Application/Traffic/CBR]
-#$e_app(1) attach-agent $src_wlan(1)
-#$e_app(1) set packet_size_ 777
-#$e_app(1) set type_ CBR
-
-#$ns connect $src_umts(2) $sink_umts(0)
-#puts [getNodeIpAddress $umts_ue(2)]
-#puts [getNodeIpAddress $umts_ue(0)]
-#puts "BS ipaddress is"
-#puts [getNodeIpAddress $BS]
-#puts "UE0 is [getNodeIpAddress $ue(0)]"
-#puts "----"
-#set e_app(2) [new Application/Traffic/CBR]
-#$e_app(2) attach-agent $src_umts(2)
-#$e_app(2) set packet_size_ 888
-#$e_app(2) set type_ CBR
-
-#$ns at 1.0 "$e_app(1) start"
-#$ns at 1.0 "$e_app(2) start"
-puts ""
-puts "initiating networks..."
-
-array set in_service_network [scanNetworks 20 ue in_service_network wlan_ue umts_ue $AP0 $BS]
+# Scanning Networks
+array set in_service_network [scanNetworks 20 ue in_service_network wifi_interface td_scdma_interface $AP0 $BS]
 
 puts "Done initiation networks"
 
 for {set i 0} {$i < 18} {incr i 1} {
 	set dst_node_index [expr $i+2]
-	if {$in_service_network($i) == $WIFI_IN_SERVICE && $in_service_network($dst_node_index) == $WIFI_IN_SERVICE} {
-		$ns connect $src_wlan($i) $sink_wlan($dst_node_index)
-		$app($i) attach-agent $src_wlan($i)
-		$ns at 10.0 "getCurrentDelay  $ue($i) $wlan_ue($dst_node_index) \"cbr\" $realtime_monitor"
-		$ns at 11.0 "getMeanDelay $ue($i) $wlan_ue($dst_node_index) \"cbr\" $realtime_monitor"
-	}
-	if {$in_service_network($i) == $UMTS_IN_SERVICE && $in_service_network($dst_node_index) == $WIFI_IN_SERVICE} {
-		$ns connect $src_umts($i) $sink_wlan($dst_node_index)
-		$app($i) attach-agent $src_umts($i)
-		$ns at 10.0 "getCurrentDelay  $ue($i) $wlan_ue($dst_node_index) \"cbr\" $realtime_monitor"
-		$ns at 11.0 "getMeanDelay $ue($i) $wlan_ue($dst_node_index) \"cbr\" $realtime_monitor"
-	}
-	if {$in_service_network($i) == $WIFI_IN_SERVICE && $in_service_network($dst_node_index) == $UMTS_IN_SERVICE} {
-		$ns connect $src_wlan($i) $sink_umts($dst_node_index)
-		$app($i) attach-agent $src_wlan($i)
-		$ns at 10.0 "getCurrentDelay  $ue($i) $umts_ue($dst_node_index) \"cbr\" $realtime_monitor"
-		$ns at 11.0 "getMeanDelay $ue($i) $umts_ue($dst_node_index) \"cbr\" $realtime_monitor"
-	}
-	if {$in_service_network($i) == $UMTS_IN_SERVICE && $in_service_network($dst_node_index) == $UMTS_IN_SERVICE} {
-		$ns connect $src_umts($i) $sink_umts($dst_node_index)
-		$app($i) attach-agent $src_umts($i)
-		$ns at 10.0 "getCurrentDelay  $ue($i) $umts_ue($dst_node_index) \"cbr\" $realtime_monitor"
-		$ns at 11.0 "getMeanDelay $ue($i) $umts_ue($dst_node_index) \"cbr\" $realtime_monitor"
-	}
+
+ 	if {$in_service_network($i) == $WIFI_IN_SERVICE && $in_service_network($dst_node_index) == $WIFI_IN_SERVICE} {
+ 		#$ns connect $src_wlan($i) $sink_wlan($dst_node_index)
+ 		$ue($i) connect-agent $src_wlan($i) $sink_wlan($dst_node_index) $wifi_interface($i)
+ 		$app($i) attach-agent $src_wlan($i)
+ 		puts "WIFI TO WIFI"
+ 	}
+ 	if {$in_service_network($i) == $UMTS_IN_SERVICE && $in_service_network($dst_node_index) == $WIFI_IN_SERVICE} {
+ 		#$ns connect $src_umts($i) $sink_wlan($dst_node_index)
+ 		$ue($i) connect-agent $src_umts($i) $sink_wlan($dst_node_index) $td_scdma_interface($i)
+ 		$app($i) attach-agent $src_umts($i)
+ 		puts "UMTS TO WIFI"
+ 	}
+ 	if {$in_service_network($i) == $WIFI_IN_SERVICE && $in_service_network($dst_node_index) == $UMTS_IN_SERVICE} {
+ 		#$ns connect $src_wlan($i) $sink_umts($dst_node_index)
+ 		$ue($i) connect-agent $src_wlan($i) $sink_umts($dst_node_index) $wifi_interface($i)
+ 		$app($i) attach-agent $src_wlan($i)
+ 		puts "WIFI TO UMTS"
+ 	}
+ 	if {$in_service_network($i) == $UMTS_IN_SERVICE && $in_service_network($dst_node_index) == $UMTS_IN_SERVICE} {
+ 		#$ns connect $src_umts($i) $sink_umts($dst_node_index)
+ 		$ue($i) connect-agent $src_umts($i) $sink_umts($dst_node_index) $td_scdma_interface($i)
+ 		$app($i) attach-agent $src_umts($i)
+ 		puts "UMTS TO UMTS"
+ 	}
 	$app($i) set packet_size_ [expr 666+$i]
 	$app($i) set type_ CBR
-	$ns at 1.0 "$app($i) start"
+	$ns at [expr 1.0+$i/5] "$app($i) start"
 }
 
 # do some kind of registration in UMTS
@@ -369,23 +376,15 @@ $ns node-config -llType UMTS/RLC/AM \
 
 # for the first HS-DSCH, we must create. If any other, then use attach-hsdsch
 
-$ns create-hsdsch $umts_ue(0) $src_umts(0)
+$ns create-hsdsch $td_scdma_interface(0) $src_umts(0)
 for {set i 1} {$i < 19} {incr i 1} {
-	#$ns attach-hsdsch $umts_ue(1) $src_umts(1)
-	#$ns attach-hsdsch $umts_ue(1) $sink_umts(1)
-	$ns attach-hsdsch $umts_ue($i) $src_umts($i)
-	$ns attach-hsdsch $umts_ue($i) $sink_umts($i)
+	#$ns attach-hsdsch $td_scdma_interface(1) $src_umts(1)
+	#$ns attach-hsdsch $td_scdma_interface(1) $sink_umts(1)
+	$ns attach-hsdsch $td_scdma_interface($i) $src_umts($i)
+	$ns attach-hsdsch $td_scdma_interface($i) $sink_umts($i)
 }
 puts "Done umts registration"
 
-#puts "create hsdsch umts_ue(1) sink(1)"
-
-#for {set i 2} {$i < 20} {incr i 2} {
-#	$ns attach-hsdsch $umts_ue($i) $src($i)
- # puts "attach hsdsch to umts_ue($i) src($i)"
-	#$ns attach-hsdsch $umts_ue([expr $i+1]) $sink([expr $i+1])
-  #puts "attach hsdsch umts_ue([expr $i+1]) sink([expr $i+1])"
-#}
 # we must set the trace for the environment. If not, then bandwidth is reduced and
 # packets are not sent the same way (it looks like they are queued, but TBC)
 puts "set trace for the environment......"
@@ -400,21 +399,14 @@ $BS loadSnrBlerMatrix "SNRBLERMatrix"
 $BS trace-outlink $f 2
 
 #for {set i 0} {$i < 20} {incr i 1} {
-#  $umts_ue($i) trace-inlink $f $i
-#  $umts_ue($i) trace-outlink $f $i
-#  $wlan_ue($i) trace-outlink $f [expr $i + 20]
-#  $wlan_ue($i) trace-inlink $f [expr $i + 20]
+#  $td_scdma_interface($i) trace-inlink $f $i
+#  $td_scdma_interface($i) trace-outlink $f $i
+#  $wifi_interface($i) trace-outlink $f [expr $i + 20]
+#  $wifi_interface($i) trace-inlink $f [expr $i + 20]
 #}
 
-#$ns at  2.3 "getMeanDelay  $ue(1) $umts_ue(2) \"cbr\" $realtime_monitor "
-#$ns at 12.3 "getMeanDelay  $ue(0) $wlan_ue(1) \"cbr\" $realtime_monitor "
-#$ns at 12.0 "getMeanDelay  $ue(2) $umts_ue(0) \"cbr\" $realtime_monitor"
-$ns at 12.1 "getMeanThroughput $ue(0) \"cbr\" $realtime_monitor"
-$ns at 3.0 "getDistance $wlan_ue(1) $wlan_ue(4)"
-$ns at 0.1 "record $wlan_ue(1)"
-
 # call to redirect traffic
-#$ns at 2 "redirectTraffic $ue(0) $wlan_ue(0) $src_wlan(0) $sink_wlan(1)"
+#$ns at 2 "redirectTraffic $ue(0) $wifi_interface(0) $src_wlan(0) $sink_wlan(1)"
 #$ns at 2 "$ns trace-annotate \"Redirecting traffic\""
 
 $ns at 15 "finish"
---
+++ examples/topo.tcl
@@ -0,0 +1,30 @@
+# Define prameters
+set opt(BS_X)		500
+set opt(BS_Y) 		500	
+set opt(AP_X) 		650
+set opt(AP_Y)		500
+set opt(GGSN_X) 	500
+set opt(GGSN_Y)		900
+set opt(RNC_X)		650
+set opt(RNC_Y) 		700
+set opt(SGSN_X)		700
+set opt(SGSN_Y) 	750
+
+set opt(Yahoo_X)	500
+set opt(Yahoo_Y)	[expr 900-170]
+set opt(Facebook_X)	[expr 500-125]
+set opt(Facebook_Y) 900
+set opt(QQ_X) 		500
+set opt(QQ_Y) 		[expr 900+150]
+set opt(JingFm_X)	[expr 500+150]
+set opt(JingFm_Y)   900
+set opt(Steam_X)	[expr 500+125]
+set opt(Steam_Y)	[expr 900-125]
+set opt(Youtube_X)	[expr 500-125]
+set opt(Youtube_Y)	[expr 900-125]
+set opt(Google_X) 	[expr 500-125]
+set opt(Google_Y) 	[expr 900+125]
+
+set opt(node_size) 	15
+
+
